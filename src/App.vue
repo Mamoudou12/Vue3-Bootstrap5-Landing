8a33a8abc3bd783b7734eaeac7e935b5181234ec
Mamoudou12 <bmamoudou463@gmail.com>
2024-08-09 10:11:14 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Navbar from './components/Navbar.vue'
+import Jumbotron from './components/Jumbotron.vue'
+export default {
+  name: "App", 
+  components: {
+      Jumbotron
+    }
+  
+}
+</script>
+
+<template>
+  <Navbar />
+  <jumbotoron />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Jumbotoron.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  name: "Jumbotoron"
+}
+</script>
+
+<template>
+ <div class="container">
+  <div class="row">
+    <div class="col-md-7">
+      <!-- <img src="https://picsum.photos/id/237/200/300 " alt=""> -->
+      Image
+    </div>
+    <div class="col-md-5">
+      <h1 class="font-weight-light">Tagline</h1>
+      <p class="mt-4">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius voluptatibus rerum impedit quos sequi magnam accusantium sunt consequatur, accusamus dolorem? Autem animi iusto laudantium sapiente laboriosam voluptatem eos, ea quis!
+      </p>
+      <button type="button" class="btn btn-primary">Call to action</button>
+    </div>
+  </div>
+ </div>
+</template>
+
+<style scoped>
+</style>
